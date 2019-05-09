9c76cf1352e546b016ae653220bfccf5a808bde8
moniet <moniet.sawhney@gmail.com>
2019-05-09 01:14:10 +0100

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
-
+def oxford_comma(arr)
+  len = arr.length
+  if len == 1
+    return arr.join
+  elsif len == 2
+    return arr.join(' and ')
+  else
+    arr[-1] = "and #{arr[-1]}"
+    arr.join(', ')
+  end
 end
